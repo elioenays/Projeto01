564f91de88f68a9f8538048a975d697877897897
Eli <elionay407@gmail.com>
2023-01-13 15:50:20 -0300

    criando bd json

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import http from 'http'
 
 import crypto from 'crypto'
+import { Database } from '../database.js'
 import { json } from './middlewares/json.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -11,13 +12,16 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
     return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({ id: crypto.randomUUID(), name, email })
+    const user = { id: crypto.randomUUID(), name, email }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
