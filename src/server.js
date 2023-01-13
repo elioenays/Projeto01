c9de7b2c9cd018d4f4317076c646f8f2736dcefa
Eli <elionay407@gmail.com>
2023-01-13 18:38:40 -0300

    Separando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -1,29 +1,19 @@
 import http from 'http'
 
-import crypto from 'crypto'
-import { Database } from './database.js'
 import { json } from './middlewares/json.js'
-
-const database = new Database()
+import { routes } from './routes.js'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-    return res.end(JSON.stringify(users))
-  }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = { id: crypto.randomUUID(), name, email }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
