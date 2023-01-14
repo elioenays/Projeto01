b234e0b91fc954c58a97bbfffa771dfe9ebf2fff
Eli <elionay407@gmail.com>
2023-01-13 23:27:32 -0300

    Capturando query parameters

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from 'http'
 
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParam } from './utils/extract-query-param.js'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -15,7 +16,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path)
 
-    req.params = { ...routeParams.groups }
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParam(query) : {}
 
     return route.handler(req, res)
   }
